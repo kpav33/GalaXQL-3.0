4173ddd02b68b8f5c923f23d854726fe284ac077
Klemen <klemenpavlovic@gmail.com>
2021-02-08 13:06:50 +0100

    Add solutions

---
+++ 06-INSERT-INTO-VALUES.sql
@@ -0,0 +1,8 @@
+--Find stars that fit the requirements
+SELECT * FROM stars WHERE starid > 5000 AND starid < 15000 AND class = 7;
+--Insert five stars into the table
+INSERT INTO hilight VALUES(5007);
+INSERT INTO hilight VALUES(5048);
+INSERT INTO hilight VALUES(5050);
+INSERT INTO hilight VALUES(5068);
+INSERT INTO hilight VALUES(5075);
